832f987c90ff09eeb2138734f83f02c6b1a9716c
Dharmender <112612895+dharmender12@users.noreply.github.com>
2025-08-11 21:18:45 +0530

    Add files via upload

---
+++ ASSIGNMENT 2/task1_math_operations.py
@@ -0,0 +1,31 @@
+##### Task 1: Perform Basic Mathematical Operations
+# **Problem Statement: Write a Python program that does the following:**
+
+# 1.  Takes two numbers as input from the user.
+
+
+a = input('Enter the first number : ')
+b = input('Enter the second number : ')
+
+a = float(a)
+b = float(b)
+
+# 2.  Performs the basic mathematical operations on these two numbers:
+#     -	Addition
+#     -	Subtraction
+#     -	Multiplication
+#     -	Division
+
+addition = a + b 
+substraction = a - b 
+multiplication = a * b
+division = a / b 
+
+# 3.  Displays the results of each operation on the screen.
+
+print(f"Addition : {addition}")
+print(f'substraction : {substraction}')
+print(f'multiplication : {multiplication}')
+print(f'division : {division}')
+print('')
+
---
+++ ASSIGNMENT 2/task2_personalized_greeting.py
@@ -0,0 +1,12 @@
+### Task 2: Create a Personalized Greeting
+# **Problem Statement:** Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+
+f_name = input('Enter your first name: ')
+l_name = input('Enter your last name: ')
+
+full_name = f_name + ' ' + l_name
+
+print(f'Hello, {full_name}! welcom to the Python Program.')
